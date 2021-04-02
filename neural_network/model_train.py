00c602e30c66a3d977b86fd79526091c8ebe7e2d
huangxh <1911036090@qq.com>
2021-04-02 21:52:40 +0800

    Transfer Learning

--- neural_network/model_train.py
+++ neural_network/model_train.py
@@ -1,12 +1,14 @@
 from ast import increment_lineno
 import matplotlib
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.python.keras import activations
+from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.layers import Conv2D,Flatten,MaxPooling2D,Dense
+from tensorflow.keras.layers import Conv2D,Flatten,MaxPooling2D,Dense,GlobalAveragePooling2D
 
 #主要调整参数
 train_file_path=r'D:\python_work\Garbage_Classification\neural_network\dataset\image_train'
@@ -14,7 +16,7 @@ validation_file_path=r'D:\python_work\Garbage_Classification\neural_network\data
 img_width,img_height = 256,256
 batch_size = 16
 label_num=8
-train_epochs=25
+train_epochs=5
 
 train_datagen = ImageDataGenerator(
        rescale=1./255,          #归一化
@@ -46,29 +48,47 @@ val_generator=val_datagen.flow_from_directory(
        save_format="jpg"
 )
 
-model=tf.keras.Sequential()
-model.add(Conv2D(filters=32,kernel_size=3,padding='same',activation='relu',input_shape=(256,256,3))) #多维数据扁平化，变成一维  
-model.add(MaxPooling2D(pool_size=2))
 
-model.add(Conv2D(filters=64,kernel_size=3,padding='same',activation='relu'))
-model.add(MaxPooling2D(pool_size=2))
+from tensorflow.keras.applications.xception import Xception
+base_model=Xception(
+       include_top=False,
+       weights="imagenet",
+       input_shape=(img_height,img_width,3),
+       pooling='avg'
+)
+base_model.trainable=False
+base_model.summary()
+print("模型网络定义：{}层".format(len(base_model.layers)))
 
-# model.add(Conv2D(filters=32,kernel_size=3,padding='same',activation='relu'))
-# model.add(MaxPooling2D(pool_size=2))
-
-# model.add(Conv2D(filters=32,kernel_size=3,padding='same',activation='relu'))
-# model.add(MaxPooling2D(pool_size=2))
-
-model.add(Flatten())
-#model.add(Dense(400,activation='relu'))
-model.add(Dense(128,activation='relu'))
-model.add(tf.keras.layers.Dropout(0.2)) #添加dropout层，抑制过拟合
-model.add(Dense(84,activation='relu'))
-model.add(tf.keras.layers.Dropout(0.2)) #添加dropout层，抑制过拟合
+model=keras.Sequential()
+model.add(base_model)
+model.add(Dense(512,activation='relu'))
 model.add(Dense(label_num,activation='softmax'))
 model.summary()
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),loss='categorical_crossentropy',metrics=['acc'])
+# model=tf.keras.Sequential()
+# model.add(Conv2D(filters=32,kernel_size=3,padding='same',activation='relu',input_shape=(256,256,3))) #多维数据扁平化，变成一维  
+# model.add(MaxPooling2D(pool_size=2))
+
+# model.add(Conv2D(filters=64,kernel_size=3,padding='same',activation='relu'))
+# model.add(MaxPooling2D(pool_size=2))
+
+# # model.add(Conv2D(filters=32,kernel_size=3,padding='same',activation='relu'))
+# # model.add(MaxPooling2D(pool_size=2))
+
+# # model.add(Conv2D(filters=32,kernel_size=3,padding='same',activation='relu'))
+# # model.add(MaxPooling2D(pool_size=2))
+
+# model.add(Flatten())
+# #model.add(Dense(400,activation='relu'))
+# model.add(Dense(1024,activation='relu'))
+# model.add(tf.keras.layers.Dropout(0.2)) #添加dropout层，抑制过拟合
+# model.add(Dense(84,activation='relu'))
+# model.add(tf.keras.layers.Dropout(0.2)) #添加dropout层，抑制过拟合
+# model.add(Dense(label_num,activation='softmax'))
+# model.summary()
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),loss='categorical_crossentropy',metrics=['acc'])
 print("model training...")
 history=model.fit(train_generator,epochs=train_epochs,validation_data=val_generator) 
 
